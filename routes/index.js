fe9a38c189415f5b849ab7a48c9f8c11c51755fb
Marion <thm.marion@gmail.com>
2023-01-26 15:23:13 +0100

    route get message from user

--- routes/index.js
+++ routes/index.js
@@ -37,5 +37,16 @@ router.post('/message', (req, res) => {
     
 });
 
+router.get('/messageFromUser/:author', (req, res) => {
+  Chat.find({author : req.params.author}).then((data) => {
+    if(!data){
+      res.json({result : false})
+    }
+    else {
+      res.json({result : true})
+    }
+  })
+})
+
 
 module.exports = router;
